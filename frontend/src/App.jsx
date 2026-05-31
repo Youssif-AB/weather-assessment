f2f6bd8d76e2f9f41465e7d0129c8cdd4f9f0368
30208726 <youssif.abdelaziz1@ucalgary.ca>
2026-05-30 23:00:54 -0600

    Added basic five day forecast display

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,22 @@ function App() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState(null);
   const [error, setError] = useState("");
+  const [forecast, setForecast] = useState([]);
+
+  const fetchForecast = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (!response.ok) {
+      return;   
+    }
+
+    const dailyForecast = data.list.filter((item, index) => {
+        return index % 8 === 0;
+    });
+    
+    setForecast(dailyForecast)
+  }
 
   const fetchWeather = async (url) => {
     const response = await fetch(url)
@@ -22,10 +38,16 @@ function App() {
   const handleSearch = async () => {
     const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
-    const url = 
+    const weatherUrl = 
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-    await fetchWeather(url)
+    const forecastUrl =
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+
+
+
+    await fetchWeather(weatherUrl)
+    await fetchForecast(forecastUrl)
   };
 
   const handleCurrentLocation = () => {
@@ -75,6 +97,20 @@ function App() {
           <p>Temperature: {weather.main.temp}C</p>
           <p>Real Feel: {weather.main.feels_like}</p>
 
+        </div>
+      )}
+
+      {forecast.length > 0 && (
+        <div>
+          <h2>5 Day Forecast</h2>
+
+          {forecast.map((day) => (
+            <div key={day.dt}>
+              <p>{day.dt}</p>
+              <p>{day.main.temp}</p>
+              <p>{day.weather[0].description}</p>
+            </div>
+          ))}
         </div>
       )}
     </main>
